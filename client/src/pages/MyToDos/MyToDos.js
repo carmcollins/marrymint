728d4db8e985c18cd44f4808ccc33b6137918268
Carmen Collins <hellocarmencollins@gmail.com>
2018-09-27 09:49:09 -0500

    small edits to to do list styling and code formatting

--- client/src/pages/MyToDos/MyToDos.js
+++ client/src/pages/MyToDos/MyToDos.js
@@ -3,60 +3,208 @@ import styles from "./MyToDos.css";
 
 const ToDos = () =>
     <div className="container">
-        <h4 className="subtitle">10-12 Months Out</h4>
-            <ul class="collection with-header">
-                <li className="collection-item"><div>Collect design inspiration<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Set a budget<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Book ceremony and reception venues<a className="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Compile your guest list and collect addresses<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Choose your wedding party<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Book a photographer and videographer<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Schedule engagement photos<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
+        <h4 className="subtitle center">10-12 Months Out</h4>
+            <ul className="collection">
+                <li className="collection-item">
+                    <div>Collect design inspiration
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Set a budget
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Book ceremony and reception venues
+                    <a className="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Compile your guest list and collect addresses
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Choose your wedding party
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Book a photographer and videographer
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Schedule engagement photos
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
             </ul>
-        <h4 className="subtitle">6-9 Months Out</h4>
-            <ul className="collection with-header">
-                <li className="collection-item"><div>Shop for a wedding dress, shoes, and accessories<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Choose a ceremony officiant<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Reserve hotel blocks for out of town guests<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Book music for ceremony and reception<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Hire a florist<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Book a caterer and select menu<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Design stationery suite and paper goods<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Reserve rentals for ceremony and reception<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
+        <h4 className="subtitle center">6-9 Months Out</h4>
+            <ul className="collection">
+                <li className="collection-item">
+                    <div>Shop for a wedding dress, shoes, and accessories
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Choose a ceremony officiant
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Reserve hotel blocks for out of town guests
+                    <a className="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Book music for ceremony and reception
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Hire a florist
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Book a caterer and select menu
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Design stationery suite and paper goods
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Reserve rentals for ceremony and reception
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
             </ul>
-        <h4 className="subtitle">4-6 Months Out</h4>
-            <ul className="collection with-header">
-                <li className="collection-item"><div>Set up a registry<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Send out save the dates<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Shop for groom and wedding party attire<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Buy wedding bands<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Schedule cake tastings and book baker<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Plan rehearsal dinner<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Organize honeymoon<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
+        <h4 className="subtitle center">4-6 Months Out</h4>
+            <ul className="collection">
+                <li className="collection-item">
+                    <div>Set up a registry
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Send out save the dates
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Shop for groom and wedding party attire
+                    <a className="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Buy wedding bands
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Schedule cake tastings and book baker
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Plan rehearsal dinner
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Organize honeymoon
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
             </ul>
-        <h4 className="subtitle">2-4 Months Out</h4>
-            <ul className="collection with-header">
-                <li className="collection-item"><div>Schedule hair and makeup trials and book stylists<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Secure transportation for guests<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Hire lighting specialist<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Meet with officiant and plan ceremony service<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Send out invitations<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
+        <h4 className="subtitle center">2-4 Months Out</h4>
+            <ul className="collection">
+                <li className="collection-item">
+                    <div>Schedule hair and makeup trials and book stylists
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Secure transportation for guests
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Hire lighting specialist
+                    <a className="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Meet with officiant and plan ceremony service
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Send out invitations
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
             </ul>
-        <h4 className="subtitle">1-2 Months Out</h4>
-            <ul className="collection with-header">
-                <li className="collection-item"><div>Obtain marriage license<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Schedule dress fitting with shoes and accessories<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Arrange seating chart<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Book hair cut and color<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Schedule manicure and pedicure<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Prepare toasts for rehearsal dinner and wedding reception<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
-                <li className="collection-item"><div>Prepare final payment and gratuity envelopes for vendors<a href="#!" className="secondary-content"><i className="material-icons">done</i></a></div></li>
+        <h4 className="subtitle center">1-2 Months Out</h4>
+            <ul className="collection">
+                <li className="collection-item">
+                    <div>Obtain marriage license
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Schedule dress fitting with shoes and accessories
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Arrange seating chart
+                    <a className="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Book hair cut and color
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Schedule manicure and pedicure
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Prepare toasts for rehearsal dinner and wedding reception
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Prepare final payment and gratuity envelopes for vendors
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
             </ul>
-        <h4 className="subtitle">Completed!</h4>
-            <ul className="collection with-header">
-                <li className="collection-item"><div>Completed 1</div></li>
-                <li className="collection-item"><div>Completed 2</div></li>
-                <li className="collection-item"><div>Completed 3</div></li>
+        <h4 className="subtitle center">Completed!</h4>
+            <ul className="collection">
+                <li className="collection-item">
+                    <div>Completed 1
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Completed 2
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
+                <li className="collection-item">
+                    <div>Completed 3
+                    <a href="#!" className="secondary-content"><i className="material-icons">done</i></a>
+                    </div>
+                </li>
             </ul>
     </div>
 ;
