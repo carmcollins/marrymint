cc0d8024ee13fe261b548737e7d69d7d9dc68ccc
Carmen Collins <hellocarmencollins@gmail.com>
2018-10-01 11:41:23 -0500

    deleting dashboard page and adding log out button to nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -16,24 +16,24 @@ class Nav extends Component {
                         {/* Main Nav */}
                         <nav className="navbar-fixed">
                             <div className="nav-wrapper teal lighten-3">
-                            <a href="/dashboard" className="brand-logo">marrymint</a>
-                            <a href="/dashboard" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                            <a href="" className="brand-logo">marrymint</a>
+                            <a href="" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                             <ul className="right hide-on-med-and-down">
-                                <li><Link to="/dashboard">Dashboard</Link></li>
                                 <li><Link to="/to-do-list">To Do List</Link></li>
                                 <li><Link to="/rsvp">RSVPs</Link></li>
                                 <li><Link to="/vendors">Vendors</Link></li>
                                 <li><Link to="/find-vendors">Find Vendors</Link></li>
+                                <li><Link to="/">Log Out</Link></li>
                             </ul>
                             </div>
                         </nav>
                         {/* Mobile Nav */}
                         <ul className="sidenav teal lighten-3" id="mobile-demo">
-                            <li><Link to="/dashboard">Dashboard</Link></li>
                             <li><Link to="/to-do-list">To Do List</Link></li>
                             <li><Link to="/rsvp">RSVPs</Link></li>
                             <li><Link to="/vendors">Vendors</Link></li>
                             <li><Link to="/find-vendors">Find Vendors</Link></li>
+                            <li><Link to="/">Log Out</Link></li>
                         </ul>
                     </div>
                     ) : (
--- client/src/pages/RSVPs/RSVPs.js
+++ client/src/pages/RSVPs/RSVPs.js
@@ -6,7 +6,7 @@ const RSVPs = () =>
         <Heading title="RSVPs" subtitle="Input all of your wedding guests' names into our database, and easily track their attendance as your invite responses come in." />
         <div className="container">
             <div className="row">
-                <div className="col s4">
+                <div className="col s12 m12 l4">
                     <h4 className="subtitle center">Invited</h4>
                     <ul className="collection">
                         <li className="collection-item">
@@ -29,14 +29,14 @@ const RSVPs = () =>
                         </li>
                     </ul>
                 </div>
-                <div className="col s4">
+                <div className="col s12 m6 l4">
                     <h4 className="subtitle center">Attending</h4>
                     <ul className="collection">
                         <li className="collection-item">Carmen Collins</li>
                         <li className="collection-item">Jenni Bazelak</li>
                     </ul>
                 </div>
-                <div className="col s4">
+                <div className="col s12 m6 l4">
                     <h4 className="subtitle center">Not Attending</h4>
                     <ul className="collection">
                         <li className="collection-item">Melissa Womack</li>
