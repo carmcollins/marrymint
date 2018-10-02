9223387f3e1e48d45741ef4f8746d34ca0cb5e14
Baz <jennigoldstein@gmail.com>
2018-10-02 15:09:49 -0500

    fixed RSVP div prob

--- client/src/pages/RSVPs/RSVPs.js
+++ client/src/pages/RSVPs/RSVPs.js
@@ -16,7 +16,7 @@ class RSVPs extends Component {
     };
 
     componentDidMount() {
-       
+
         this.getRSVPS()
     };
 
@@ -78,84 +78,87 @@ class RSVPs extends Component {
 
     render() {
 
-        return (   
-    <div>
-    <Heading bg="rsvpHead" title="RSVPs" subtitle="Input all of your wedding guests' names into our database, and easily track their attendance as your invite responses come in." />
-    <div className="container">
-        <div className="row">
-            <div className="col s12 m12 l12">
-                <div className="row">
-                    <form className="col s12">
-                        <div className="form-div">
-                            <h4 className="red-text text-accent-1">Add Guests Here</h4>
+        return (
+            <div>
+                <Heading bg="rsvpHead" title="RSVPs" subtitle="Input all of your wedding guests' names into our database, and easily track their attendance as your invite responses come in." />
+                <div className="container">
+                    <div className="row">
+                        <div className="col s12 m12 l12">
                             <div className="row">
-                                <form className="col s12">
+                                <div className="col s12">
                                     <div className="form-div">
                                         <h4 className="red-text text-accent-1">Add Guests Here</h4>
                                         <div className="row">
-                                            <div className="input-field col s12">
-                                                <input id="guest-name" type="text" className="validate" name="name" value={this.state.name}
-                                                    onChange={this.handleInputChange} />
-                                                <label htmlFor="guest-name">Name</label>
-                                            </div>
-                                        </div>
-                                        <br />
-                                        <div className="center">
-                                            <a id="addGuest-btn" className="waves-effect waves-teal btn teal lighten-3"
-                                                onClick={this.handleFormSubmit}>Add Guest</a>
+                                            <form className="col s12">
+                                                <div className="form-div">
+                                                    <h4 className="red-text text-accent-1">Add Guests Here</h4>
+                                                    <div className="row">
+                                                        <div className="input-field col s12">
+                                                            <input id="guest-name" type="text" className="validate" name="name" value={this.state.name}
+                                                                onChange={this.handleInputChange} />
+                                                            <label htmlFor="guest-name">Name</label>
+                                                        </div>
+                                                    </div>
+                                                    <br />
+                                                    <div className="center">
+                                                        <a id="addGuest-btn" className="waves-effect waves-teal btn teal lighten-3"
+                                                            onClick={this.handleFormSubmit}>Add Guest</a>
+                                                    </div>
+                                                </div>
+                                            </form>
                                         </div>
                                     </div>
-                                </form>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col s12 m12 l4">
+                                    <h4 className="subtitle center">Invited</h4>
+                                    <ul className="collection">
+                                        {this.state.invited.length ? (
+                                            <List>
+                                                {this.state.invited.map(invited => (
+                                                    <li className="collection-item">
+                                                        <div>{invited.name}
+                                                            <a href="" className="secondary-content rsvp-no" onClick={this.handleNoAttend}><i className="material-icons">clear</i></a>
+                                                            <a href="" className="secondary-content rsvp-yes"><i className="material-icons" onClick={this.handleAttend(invited._id)}>done</i></a>
+                                                        </div>
+                                                    </li>
+                                                ))}
+                                            </List>
+                                        ) : (<h2 className="text-center">{this.state.message}</h2>)}
+                                    </ul>
+                                </div>
+                                <div className="col s12 m6 l4">
+                                    <h4 className="subtitle center">Attending</h4>
+                                    <ul className="collection">
+                                        {this.state.attending.length ? (
+                                            <List>
+                                                {this.state.attending.map(attending => (
+                                                    <li className="collection-item">{attending.name}</li>
+                                                ))}
+                                            </List>
+                                        ) : (<h2 className="text-center">{this.state.message}</h2>)}
+                                    </ul>
+                                </div>
+                                <div className="col s12 m6 l4">
+                                    <h4 className="subtitle center">Not Attending</h4>
+                                    <ul className="collection">
+                                        {this.state.notAttending.length ? (
+                                            <List>
+                                                {this.state.notAttending.map(notAttending => (
+                                                    <li className="collection-item">{notAttending.name}</li>
+                                                ))}
+                                            </List>
+                                        ) : (<h2 className="text-center">{this.state.message}</h2>)}
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col s12 m12 l4">
-                            <h4 className="subtitle center">Invited</h4>
-                            <ul className="collection">
-                                {this.state.invited.length ? (
-                                    <List>
-                                        {this.state.invited.map(invited => (
-                                            <li className="collection-item">
-                                                <div>{invited.name}
-                                                    <a href="" className="secondary-content rsvp-no" onClick={this.handleNoAttend}><i className="material-icons">clear</i></a>
-                                                    <a href="" className="secondary-content rsvp-yes"><i className="material-icons" onClick={this.handleAttend(invited._id)}>done</i></a>
-                                                </div>
-                                            </li>
-                                        ))}
-                                    </List>
-                                ) : (<h2 className="text-center">{this.state.message}</h2>)}
-                            </ul>
-                        </div>
-                        <div className="col s12 m6 l4">
-                            <h4 className="subtitle center">Attending</h4>
-                            <ul className="collection">
-                                {this.state.attending.length ? (
-                                    <List>
-                                        {this.state.attending.map(attending => (
-                                            <li className="collection-item">{attending.name}</li>
-                                        ))}
-                                    </List>
-                                ) : (<h2 className="text-center">{this.state.message}</h2>)}
-                            </ul>
-                        </div>
-                        <div className="col s12 m6 l4">
-                            <h4 className="subtitle center">Not Attending</h4>
-                            <ul className="collection">
-                                {this.state.notAttending.length ? (
-                                    <List>
-                                        {this.state.notAttending.map(notAttending => (
-                                            <li className="collection-item">{notAttending.name}</li>
-                                        ))}
-                                    </List>
-                                ) : (<h2 className="text-center">{this.state.message}</h2>)}
-                            </ul>
-                        </div>
-                    </div>
                 </div>
-            </div >
-        )
-    }
-};
-
+            </div>
+                        )
+                    }
+                };
+                
 export default RSVPs;
