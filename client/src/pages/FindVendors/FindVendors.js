a22fda6e54b2ac58ce171710e4a05760bda8c1cf
Carmen Collins <hellocarmencollins@gmail.com>
2018-10-03 11:59:33 -0500

    adding in filter buttons on find vendors page

--- client/src/pages/FindVendors/FindVendors.js
+++ client/src/pages/FindVendors/FindVendors.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Heading from "../../components/Heading";
+import "./FindVendors.css";
 
 const FindVendors = () =>
     <div>
         <Heading bg="findVendorsHead" title="Find Vendors" subtitle="We've hand-selected all of our favorite vendors in town to ensure that you'll have the best team working with you on your wedding day." />
         <div className="container">
             <h4 className="subtitle center">Find Vendors</h4>
+            <div className="filter-btns">
+                <a href="/find-vendors" class="btn-small filter-btn red accent-1">All</a>
+                <a href="/find-vendors/planners" class="btn-small filter-btn red accent-1">Planners</a>
+                <a href="/find-vendors/photographers" class="btn-small filter-btn red accent-1">Photographers</a>
+                <a href="/find-vendors/florists" class="btn-small filter-btn red accent-1">Florists</a>
+                <a href="/find-vendors/rentals" class="btn-small filter-btn red accent-1">Rentals</a>
+                <a href="/find-vendors/venues" class="btn-small filter-btn red accent-1">Venues</a>
+                <a href="/find-vendors/caterers" class="btn-small filter-btn red accent-1">Caterers</a>
+                <a href="/find-vendors/bakers" class="btn-small filter-btn red accent-1">Bakers</a>
+                <a href="/find-vendors/stationery" class="btn-small filter-btn red accent-1">Stationery</a>
+                <a href="/find-vendors/videographers" class="btn-small filter-btn red accent-1">Videographers</a>
+                <a href="/find-vendors/beauty" class="btn-small filter-btn red accent-1">Beauty</a>
+            </div>
             <div className="row">
                 <div className="col s12 m4">
                     <div className="card">
